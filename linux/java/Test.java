8e820d3d4350e362fd566031418081a01ada8eee
unibosy <unibosy@gmail.com>
2018-05-18 12:19:23 +0800

    update

--- linux/java/Test.java
+++ linux/java/Test.java
@@ -1,12 +1,8 @@
-import a.common.*;
-import b.common.*;
 
-class mainTest{
+class Test{
 
     public static void main(String[] argv){
  
-        A a = new A();
-        B b = new B();
         byte[] bigObject = new byte[1024 * 1024 * 500];
 
         System.out.println("test over");
