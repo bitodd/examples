164073d14a9f44782807d3e9409e7606f0c95922
unibosy <unibosy@gmail.com>
2018-06-05 20:35:27 +0800

    test return local var pointer

---
+++ linux/cpp/testStr.cpp
@@ -0,0 +1,16 @@
+#include "string"
+#include "iostream"
+using namespace std;
+
+char* testChar(){
+    char* test = "hello";
+    //char test = "hellp";
+    return test;
+}
+int main(void) {
+
+    char* ret = testChar();
+    cout<<"ret:"<<ret<<endl;
+    return 0;
+
+}
