d192fc9337b3ee69a9fe98457093f32fc682bdf7
unibosy <unibosy@gmail.com>
2018-07-02 20:26:20 +0800

    add function pointer

--- linux/c/func_pointer.c
+++ linux/c/func_pointer.c
@@ -5,11 +5,17 @@ int func(int a, int b)
 {
     printf("\n var a:%d, b:%d\n", a, b);
 }
-
+//define a function pointer
 int (*myfun)(int, int);
+//defind a type of function pointer
+typedef int (*MYFUNC)(int, int);
 
 int main(int argc, char* argv[]){
     myfun = func;
     myfun(10,12);
+
+    MYFUNC myfun2;
+    myfun2 = func;
+    myfun2(13,14);
     return 0;
 }
